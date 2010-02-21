3e99538ba53b3187fafd104f8bc564399148b67b
Tom Moertel <tom@moertel.com>
2010-02-20 21:40:22 -0500

    Made changes prior to publication.

--- mtlsd-performance-and-efficiency.R
+++ mtlsd-performance-and-efficiency.R
@@ -7,8 +7,12 @@
 ##
 ## Primary sources:  2008-09 PSSA data
 ##
-## Analyis by Tom Moertel <tom@mlao.org>.
+## Analysis by the Mt. Lebanon Accountability Organization
+## Tom Moertel <tom@mlao.org>
+## http://www.mlao.org/
+##
 ## 2009-11-01
+## Revised 2010-02-20
 ##
 ## This analysis is an R program:  http://www.r-project.org/
 ##=============================================================================
@@ -17,14 +21,22 @@
 
 options(digits = 2)
 
-
 require("ggplot2")
 
+##=============================================================================
+## Configuration
+##=============================================================================
+
+
+## Graphical preferences
 
 interest.alpha <- 0.5
 other.alpha <- 0.25
 other.color <- "darkgrey"
 other.SD <- "Other"
+
+
+## Which schools to highlight
 
 school.districts.of.interest <- local({
   x <- matrix(ncol=4, byrow=T,
@@ -67,7 +79,9 @@ pssa.s  <- load.data.set("data/PSSA_Results_Science_School_2009.csv")
 
 ## Next, I merge the data sets into a single comprehensive data set
 
-pssa <- merge(merge(pssa.mr, pssa.w, all=T), pssa.s, all=T)
+merge.pssa <- function(x, y)
+  merge(x, y, all = T, by = c("District", "School", "Grade"))
+pssa <- merge.pssa(merge.pssa(pssa.mr, pssa.w), pssa.s)
 
 
 ## Next, I restrict the data set to all-student data (that is, I
@@ -92,31 +106,68 @@ pssa.all <- local({
 
 
 ## Next, I extract those columns in which I am interested, mainly the
-## scores for advanced levels of performance
+## scores for advanced levels of performance.
 
 pssa.all.renamed <-
   within(pssa.all, {
-    Reading <- X..Advanced.Reading; X..Advanced.Reading <- NULL
-    Math    <- X..Advanced.Math;    X..Advanced.Math <- NULL
-    Writing <- X..Advanced.Writing; X..Advanced.Writing <- NULL
-    Science <- X..Advanced.Science; X..Advanced.Science <- NULL
+    Reading <- X..Advanced.Reading
+    Math    <- X..Advanced.Math
+    Writing <- X..Advanced.Writing
+    Science <- X..Advanced.Science
   })
+
+## ## Use the following scheme instead of the one above to replicate the
+## ## district's use of the combiniation of "proficient" and "advanced"
+## ## as their benchmark.
+##
+## pssa.all.renamed <-
+##   within(pssa.all, {
+##     Reading <- X..Advanced.Reading + X..Proficient.Reading
+##     Math    <- X..Advanced.Math    + X..Proficient.Math
+##     Writing <- X..Advanced.Writing + X..Proficient.Writing
+##     Science <- X..Advanced.Science + X..Proficient.Science
+##   })
+
+
+## Now, I "melt" the data of interest into an indexed data frame,
+## ready for use in summary graphics and statistics,
 
 pssa.all.melted <-
   melt(pssa.all.renamed,
        id=c("Grade", "District", "SD", "School"),
        measure=c("Math", "Reading", "Writing", "Science"))
 
+
+## I reduce the data to its ECDF (empirical cumulative distribution
+## function) for the variables of interest.  A variable's ECDF relates
+## its values in an absolute sense to their relative rankings.
+
 pssa.all.melted.ecdf <-
   ddply(pssa.all.melted, .(Grade, variable), function(df) {
-    xs <- na.omit(df$value)
+    xs <- na.omit(df$value)  # ignore schools that didn't participate
     usxs <- sort(unique(xs))
     usxs.ecdf <- if (length(xs) > 0) ecdf(xs)(usxs) else c()
     data.frame(value = usxs, value.ecdf = 100 * usxs.ecdf)
   })
 
+
+## I now extend the subset of the per-school statistics that are
+## "interesting" -- Mt. Lebaon, Upper St. Clair, etc. -- with their
+## ECDFs.
+
 pssa.interest.melted.ecdf <-
   merge(subset(pssa.all.melted, SD != "Other"), pssa.all.melted.ecdf)
+
+
+
+##=============================================================================
+## Summary graphic:  School Performance Rankings
+##
+## This graphic summarizes the performance of schools in both absolute
+## and relative terms, relating the portion of students who tested at
+## the advance level at each school to that school's rank among all
+## Pennsylvania schools.
+##=============================================================================
 
 plot.rankings <-
 qplot(value, value.ecdf,
@@ -131,8 +182,7 @@ qplot(value, value.ecdf,
       binwidth = 1,
       facets = Grade ~ variable
       ) +
-  geom_point(aes(colour = SD, alpha = rep(0.5, length(SD))),
-             data = pssa.interest.melted.ecdf) +
+  geom_point(aes(colour = SD), alpha = 0.5, data = pssa.interest.melted.ecdf) +
   geom_text(aes(label = School), data = pssa.interest.melted.ecdf,
             hjust = -0.10, vjust = 0.5, size = 2, angle = -60) +
   scale_colour_manual(name = "School District",
@@ -141,8 +191,6 @@ qplot(value, value.ecdf,
   scale_x_continuous(breaks=seq(20, 100, 20)) +
   scale_y_continuous(breaks=seq(20, 100, 20)) +
   scale_alpha(legend = F)
-
-print(plot.rankings)
 
 ggsave(file="mtlsd-2009-pssa-rankings.png",
        plot = plot.rankings,
@@ -153,52 +201,21 @@ ggsave(file="mtlsd-2009-pssa-rankings.pdf",
        width=8.5, height=11)
 
 
-mk.performance.histogram <- function(variable) {
-  variable.s <- substitute(variable)
-  variable.n <- sub(".*\\.", "", deparse(variable.s))
-  f <- eval(bquote(function(df) {
-    qplot(.(variable.s),
-          main = paste(sep="\n",
-            "How Mt. Lebanon compares to other Pennsylvania schools",
-            paste("Advanced", variable.n, "(2008-09 PSSA)")),
-          xlab = paste(
-            "Percentage of students demonstrating advanced skills in",
-            paste("Advanced", variable.n)),
-          ylab = "Count of schools",
-          data = df,
-          geom = "histogram",
-          binwidth = 1,
-          fill = SD,
-          facets = Grade ~ . ) +
-            scale_y_log10() +
-      scale_fill_manual(name = "School District",
-                        values = school.districts.of.interest$color,
-                        breaks = school.districts.of.interest$SD)  }))
-  f(df)
-}
-
-## rank.on(X..Advanced.Math)
-
-
 
 ##=============================================================================
-## EFFICIENCY ANALYSIS
-##
-## Now I load tuition data from the PA Dept. of Ed:
-## http://www.pde.state.pa.us/school_acct/cwp/view.asp?a=182&q=76814#tuitionrates
+## Summary graphic:  School Efficiency (Performance vs. Tuition)
 ##
 ## I will contrast the performance of each school with its tuition.
 ## More efficient schools will offer higher performance for lower tuition.
 ##=============================================================================
 
+## Now I load tuition data from the PA Dept. of Ed:
+## http://www.pde.state.pa.us/school_acct/cwp/view.asp?a=182&q=76814#tuitionrates
+
 tuition.rates <- read.csv("data/pa-tuition-rates-2008.csv", skip=5, header=T)
 
-pssa.vs.tuition <-
-  merge(pssa.all, tuition.rates, sort=T,
-        by.x = c("District"),
-        by.y = c("School.District"))
-pssa.vs.tuition <- within(pssa.vs.tuition,
-                          Tuition <- (Elementary + Secondary) / 2)
+
+## Now I merge the tuition data into the existing, melted performance data
 
 pssa.all.melted.vs.tuition <-
   merge(pssa.all.melted, tuition.rates, sort=T,
@@ -206,6 +223,9 @@ pssa.all.melted.vs.tuition <-
         by.y = c("School.District"))
 pssa.all.melted.vs.tuition <- within(pssa.all.melted.vs.tuition,
                                      Tuition <- (Elementary + Secondary) / 2)
+
+
+## And I prepare the plot.
 
 plot.ed.eff <-
 qplot(value, Tuition, data=pssa.all.melted.vs.tuition,
@@ -218,15 +238,15 @@ qplot(value, Tuition, data=pssa.all.melted.vs.tuition,
       ylab = "Tuition (lower is better)",
       alpha = I(0.25),
       colour = SD) +
-  geom_point(data=subset(pssa.all.melted.vs.tuition,
-                         SD != "Other" & SD != "MTL")) +
-  geom_point(data=subset(pssa.all.melted.vs.tuition, SD == "MTL")) +
+  geom_point(alpha = 0.5,
+             data = subset(pssa.all.melted.vs.tuition, SD != "Other")) +
   facet_grid(Grade ~ variable, margins=T) +
   scale_x_continuous(breaks=c(25, 50, 75)) +
   scale_y_continuous(formatter="dollar", breaks=c(10000,15000)) +
   scale_colour_manual(name = "School District",
                       breaks = school.districts.of.interest$SD,
-                      values = school.districts.of.interest$color)
+                      values = school.districts.of.interest$color) +
+  scale_alpha(legend = F)
 
 ggsave(file="mtlsd-2009-pssa-scores-vs-tuition.png",
        plot = plot.ed.eff,
